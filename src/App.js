a18527a499c6d5a1c9204b19f5587f8aa581cb10
esy <co6747@naver.com>
2021-09-22 20:41:46 +0900

    #material-ui 라이브러리로 frame 잡기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import ScrollTop from './ScrollTop';
 function App() {
   return (
-   <div></div>
+    <div>
+      <ScrollTop/>
+    </div>
+
   );
 }
 
