37bf9385668693ba30caf5d18e0337f1c54c18b1
esy <co6747@naver.com>
2021-09-27 16:24:38 +0900

    #Footer 추가

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Days from './Component/Days';
 import Step from './Component/Step';
 import Grid from '@mui/material/Grid';
 import FeaturedPost from './FeaturedPost';
-
+import Footer from './Footer';
 
 const featuredPosts =[
   {
@@ -69,9 +69,7 @@ export default function App() {
               <FeaturedPost key={post.title} post={post} />
             ))}
           </Grid>
-        
-
-        
+          <Footer/>
       </Container>
   </div>
   );
